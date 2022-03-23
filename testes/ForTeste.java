c0ecf35e362bb41d4e176beb7e8fdb822583b615
LARISSA NUNES VAZ ALVES DE OLIVEIRA <22102365@CFP127ALUNO.LOCAL>
2022-03-23 11:12:47 -0300

    fpoo

---
+++ testes/ForTeste.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+class ForTeste {
+    public static void main (String args []) {
+        Scanner sc = new Scanner (System.in);
+
+        for ( int i=1 ; i<=50 ; i=i+1 ) {
+            System.out.println (i);
+        }
+    }
+}
