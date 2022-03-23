9ec962cb1b3cdd9f9512e70806f38d5cf7a46eec
LARISSA NUNES VAZ ALVES DE OLIVEIRA <22102365@CFP127ALUNO.LOCAL>
2022-03-23 08:54:18 -0300

    FPOO

---
+++ Exercicios/testes/Bits.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+class Bits {
+    public static void main (String args []){
+        Scanner sc = new Scanner(System.in); 
+
+        System.out.println("Insira o valor 128:");
+        byte valor = sc.nextByte ();
+
+        System.out.println (valor);
+    }
+}
