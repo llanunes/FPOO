f898fb485b2391b25d5578d9e93f3e0262c736a7
LARISSA NUNES VAZ ALVES DE OLIVEIRA <22102365@CFP127ALUNO.LOCAL>
2022-03-28 10:24:00 -0300

    FPOO

---
+++ testes/Fatorial.java
@@ -0,0 +1,10 @@
+class Fatorial{
+    public static void main (String[]args){
+        
+        int num = 20;
+
+        for (int i = 1 ; i > 9 ; ++i) {
+            System.out.println ();
+        }
+    }
+}
---
+++ testes/Tabuada.java
@@ -0,0 +1,9 @@
+class Tabuada {
+    public static void main (String args []){
+        int n = 5;
+
+        for ( int i = 0 ; i <= 10; i++){
+            System.out.println(n + "x" + i + "=" + n*i);
+        }
+    }
+}
