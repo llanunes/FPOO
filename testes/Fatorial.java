04730ea4e9021471fcd18d6bfe7da7f75f6c1a0b
LARISSA NUNES VAZ ALVES DE OLIVEIRA <22102365@CFP127ALUNO.LOCAL>
2022-03-30 08:45:55 -0300

    FPOO

--- testes/Fatorial.java
+++ testes/Fatorial.java
@@ -1,6 +1,17 @@
+import java.util.Scanner;
+
 class Fatorial{
-    public static void main (String[]args){
-    
-        
-    }
-}         
+    public static void main (String[] args){
+        Scanner sc = new Scanner (System.in);
+
+        System.out.println("Informe o numero desejado para o fatorial:");
+        long num = sc.nextLong();
+        long fat = num;
+
+        for (long i = num-1; i>=2; i--){
+            fat = fat * i;
+        }
+
+        System.out.println(num + "! = " + fat);
+    }  
+}       
