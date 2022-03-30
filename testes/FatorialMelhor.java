62c3e94edf5d1ad91927fc6e7b9c41ac862c0f04
LARISSA NUNES VAZ ALVES DE OLIVEIRA <22102365@CFP127ALUNO.LOCAL>
2022-03-30 11:22:32 -0300

    FPOO

---
+++ testes/FatorialMelhor.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+class FatorialMelhor{
+    public static void main (final String[] args) {
+        final Scanner sc = new Scanner(System.in);
+
+        while (true) {
+            while (true) {
+                System.out.println("Informe um numero positivo: ");
+                final long n = sc.nextInt();
+                long fat = n;
+               
+    
+            if (n==0){
+                System.out.println ("0! = 1");
+                break;
+            }  
+            if  (n>0) {
+                for ( int i = 2; i < n ; i ++) {
+                    fat *= i;   
+                }
+                System.out.println (n + "! = " + fat);
+                break;
+                }
+            System.out.println("Voce inseriu um numero negativo, somente numeros positivos sao aceitos.");                       
+            }
+        }    
+    }
+    
+}
+        
