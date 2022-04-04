922311d8d7ed73d1569372f7fac48482c57b0b9a
LARISSA NUNES VAZ ALVES DE OLIVEIRA <22102365@CFP127ALUNO.LOCAL>
2022-04-04 11:24:23 -0300

    FPOO

--- Exercicios_3/Ex6.java
+++ Exercicios_3/Ex6.java
@@ -3,5 +3,20 @@ import java.util.Scanner;
 class Ex6{
     public static void main (String [] args){
         Scanner sc = new Scanner (System.in);
+
+        
+        for ( int i = 1; i<2 ; i++ ){
+            System.out.println ("Desenvolvimento de sistemas - DS1M");
+        char ch = '\u2588';  
+    System.out.println(ch);
+
+        }
+        for ( int i = 1; i<2 ; i++ ){
+            System.out.println ("Desenvolvimento de sistemas - DS1M");
+        char ch = '\u2588';  
+    System.out.println(ch);
+
+        }
+        
     }
 }
--- Exercicios_3/Ex7.java
+++ Exercicios_3/Ex7.java
@@ -3,5 +3,28 @@ import java.util.Scanner;
 class Ex7{
     public static void main (String [] args){
         Scanner sc = new Scanner (System.in);
-    }
+
+        while (true) {
+            System.out.println("Informe um numero positivo: ");
+            final long n = sc.nextInt();
+            long fat = n;
+
+        if (n==0){
+            System.out.println ("0! = 1");
+            break;
+        }  
+        if (n==1){
+            System.out.println ("1! = 1");
+            break;
+        }  
+        if  (n>0) {
+            for ( int i = 2; i < n ; i ++) {
+                fat *= i;   
+            }
+            System.out.println (n + "! = " + fat);
+            break;
+            }
+        System.out.println("Voce inseriu um numero negativo, somente numeros positivos sao aceitos.");                       
+        }
+    }    
 }
